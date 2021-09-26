dcce7669761d9ab02805a8b409eca9e4c3a6b2af
Kevin Jullien <kevin.jullien@student.vinci.be>
2021-09-26 17:31:35 +0200

    refactor - same scripts calls for listings pages are now in one php file, and it is called when needed

--- views/adminListLex.php
+++ views/adminListLex.php
@@ -59,7 +59,4 @@
         </table>
     </section>
 </section>
-<script src="<?php echo SCRIPTS_PATH ?>searchBar.js"></script>
-<script src="<?php echo SCRIPTS_PATH ?>listSelection.js"></script>
-<script src="<?php echo SCRIPTS_PATH ?>goToTopButton.js"></script>
-<script src="<?php echo SCRIPTS_PATH ?>alphabetMenu.js"></script>
+<?php require_once(VIEW_PATH . "scriptsCallsListings.php");
